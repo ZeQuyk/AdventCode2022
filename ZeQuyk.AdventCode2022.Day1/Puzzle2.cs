2c790406c5b10825a3482a85282a9f945bc00fbc
Vincent Cadieux <vincent_cadieux@outlook.com>
2022-12-03 14:18:49 -0500

    Solve Day 1 - Puzzle 2

---
+++ ZeQuyk.AdventCode2022.Day1/Puzzle2.cs
@@ -0,0 +1,19 @@
+﻿using Advent2022.Core;
+
+namespace ZeQuyk.AdventCode2022.Day1
+{
+    public class Puzzle2 : PuzzleBase
+    {
+        protected override string Solve(string input)
+        {
+            var caloriesLinesByElf = input.Split("\r\n\r\n").ToList();
+            var sums = new List<int>();
+            foreach(var caloriesLines in caloriesLinesByElf)
+            {
+                sums.Add(caloriesLines.Split("\r\n").Sum(x => int.Parse(x)));
+            }
+
+            return sums.OrderByDescending(x => x).Take(3).Sum().ToString();
+        }
+    }
+}
--- src/ZeQuyk.AdventCode2022.Console/Program.cs
+++ src/ZeQuyk.AdventCode2022.Console/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using ZeQuyk.AdventCode2022.Day1;
 
-var result = new Puzzle1().Solve();
+var result = new Puzzle2().Solve();
 
 Debug.WriteLine(result);
