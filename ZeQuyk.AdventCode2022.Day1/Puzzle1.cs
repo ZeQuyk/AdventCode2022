12417e468db7e937f2bd8014040854a4289fc1a9
Vincent Cadieux <vincent_cadieux@outlook.com>
2022-12-03 14:11:59 -0500

    Solve first puzzle

---
+++ ZeQuyk.AdventCode2022.Day1/Puzzle1.cs
@@ -0,0 +1,23 @@
+﻿using Advent2022.Core;
+
+namespace ZeQuyk.AdventCode2022.Day1
+{
+    public class Puzzle1 : PuzzleBase
+    {
+        protected override string Solve(string input)
+        {
+            var caloriesLinesByElf = input.Split("\r\n\r\n").ToList();
+            var maxCalories = 0;
+            foreach (var calorieLines in caloriesLinesByElf)
+            {
+                var sum = calorieLines.Split("\r\n").Sum(x => int.Parse(x));
+                if (sum > maxCalories)
+                {
+                    maxCalories = sum;
+                }
+            }
+
+            return maxCalories.ToString();
+        }
+    }
+}
---
+++ src/ZeQuyk.AdventCode2022.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Diagnostics;
+using ZeQuyk.AdventCode2022.Day1;
+
+var result = new Puzzle1().Solve();
+
+Debug.WriteLine(result);
